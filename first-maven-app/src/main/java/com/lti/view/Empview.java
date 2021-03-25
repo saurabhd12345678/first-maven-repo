f53435a2540c5b5f3582caf93394aba13c7a5327
saurabhd12345678 <saurabhdhule123@gmail.com>
2021-03-25 12:43:02 +0530

    modified

--- first-maven-app/src/main/java/com/lti/view/Empview.java
+++ first-maven-app/src/main/java/com/lti/view/Empview.java
@@ -7,7 +7,7 @@ public class Empview {
 	public static void main(String[] args) {
 		Employee emp=new Employee();
 		emp.setEmpid(1001);
-		emp.setEmpname("saurabh");
+		emp.setEmpname("jackson");
 		
 		System.out.println("employee id is:"+emp.getEmpid());
 		System.out.println("employee name is:"+emp.getEmpname());
